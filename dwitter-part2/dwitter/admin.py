6af478fff0fd06fe00f25321ca8c0c62d86ef27b
devwizard-Mani <127428247+Xcodes-Mani@users.noreply.github.com>
2023-04-15 23:08:49 +0530

    Update admin.py

--- dwitter-part2/dwitter/admin.py
+++ dwitter-part2/dwitter/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 
-from .models import Profile, Dweet
+from .models import Profile
 
 class ProfileInline(admin.StackedInline):
     model = Profile
@@ -15,4 +15,3 @@ admin.site.unregister(Group)
 admin.site.unregister(User)
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Dweet)
